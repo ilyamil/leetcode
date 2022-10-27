1042b30fd83d39c10ac1a51dbbf1b0c32e962e82
imilyutin <idmilyutin@gmail.com>
2022-10-27 14:21:48 +0400

    added solution to LC 62

---
+++ 0062_unique_paths.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int uniquePaths(int m, int n) {
+    vector<vector<int>> dp(m, vector<int>(n, 1));
+    for (int row = 1; row < m; row++) {
+        for (int col = 1; col < n; col++) {
+            dp[row][col] = dp[row - 1][col] + dp[row][col - 1];
+        }
+    }
+    return dp[m - 1][n - 1];
+}
+
+int main() {
+    cout << uniquePaths(1, 2);
+}
